f029efc00a4caf8f7226e5c88b93fe4f961120f6
nikocukaroslav <nikocukaroslav2006@gmail.com>
2024-01-18 16:50:43 +0200

    created 3 classes

---
+++ Sana07/Apple.cs
@@ -0,0 +1,26 @@
+﻿using Sana07;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PoductLibrary
+{
+    public class Apple : Product
+    {
+        public string Name { get; set; }
+        public int Price { get; set; }
+        public int Count { get; set; }
+        public Apple( string name, int price, int count) {
+        Name= name;
+            Price= price;
+            Count= count;
+        }
+
+        public override string Display()
+        {
+            return $"Назва товару: {Name}; Ціна товару: {Price}; Кількість одиниць товару: {Count};";
+        }
+    }
+}
---
+++ Sana07/Product.cs
@@ -0,0 +1,8 @@
+﻿namespace Sana07
+{
+    public abstract class Product
+    {
+        public string Name { get; set; }
+        public int Price {  get; set; }
+    }
+}
